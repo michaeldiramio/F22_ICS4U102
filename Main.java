1ff50d41a6347293c7877e06272ac457488a9fed
Zoetewej <120403471+jaydenzzz@users.noreply.github.com>
2022-12-13 16:23:24 +0000

    bird

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
 		ArrayList<Animal> animals = new ArrayList<>();
 
 		animals.add(new Cat());
-
+    animals.add(new Bird());
 
 		/*for(int i = 0; i < animals.size(); i++) {
 			animals.get(i).makeNoise();
