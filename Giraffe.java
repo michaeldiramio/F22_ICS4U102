9c51a54acf6ad5ddf36c275b3c92e5caa1fb694e
Michael Diramio <56129729+michaeldiramio@users.noreply.github.com>
2022-12-14 15:47:08 +0000

    Added another animal trying to create a merge conflict

---
+++ Giraffe.java
@@ -0,0 +1,7 @@
+public class Giraffe extends Animal {
+
+	public void makeNoise() {
+		System.out.println("Whatever giraffe's say. I'm too tired for this.");
+	}
+	
+}
--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ public class Main {
     animals.add(new Eagle());
     animals.add(new Fish());
     animals.add(new Jellyfish());
+		animals.add(new Giraffe());
 
 		/*for(int i = 0; i < animals.size(); i++) {
 			animals.get(i).makeNoise();
